880dc59bf16a0ff708114a772be95ba0abefa2a0
Lucas Boganin <39099403+Spacelocust@users.noreply.github.com>
2023-11-06 17:23:49 +0000

    feat: add countDuplicates function with test

--- index.ts
+++ index.ts
@@ -1,4 +1,32 @@
+import { type } from "os";
+
 const numberDices = 5;
+
+type Figure = {
+  points: number;
+};
+
+type Figures = {
+  [key: string]: Figure;
+}
+
+const figures: Figures = {
+  "brelan": {
+    points: 28,
+  },
+  "carré": {
+    points: 35,
+  },
+  "full": {
+    points: 30,
+  },
+  "grande suite": {
+    points: 40,
+  },
+  "yams": {
+    points: 50,
+  },
+}
 
 export function randomNumber(): number {
   return Math.floor(Math.random() * 6) + 1;
@@ -12,5 +40,21 @@ export function getPoints(dices: number[]): number {
   if (dices.length !== numberDices) {
     return 0;
   }
+
   return dices.reduce((acc, dice) => acc + dice, 0);
+}
+
+export function countDuplicates(arr: number[]): Record<number, number> {
+
+  if (arr.length !== numberDices) {
+    return {};
+  }
+
+  const counts: Record<number, number> = {};
+
+  arr.forEach((num) => {
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+  });
+
+  return counts;
 }
--- test/yams.test.ts
+++ test/yams.test.ts
@@ -1,36 +1,7 @@
-import { describe, expect, test } from "bun:test"
-import { getPoints, randomNumber, throwDices } from "..";
+import { describe, expect, test } from "bun:test";
 
-const figures = {
-  "brelan": {
-    points: 28,
-    need: 3,
-  },
-  "carré": {
-    points: 50,
-    need: 4,
-  },
-  "full": {
-    points: 40,
-    need: 5,
-  },
-  "petite suite": {
-    points: 30,
-    need: 4,
-  },
-  "grande suite": {
-    points: 40,
-    need: 5,
-  },
-  "yams": {
-    points: 50,
-    need: 5,
-  },
-  "chance": {
-    points: 0,
-    need: 0,
-  },
-}
+import { countDuplicates, getPoints, randomNumber, throwDices } from "..";
+
 describe("yams test", () => {
   test("random number between 1 and 6 include", () => {
     expect(randomNumber()).toBeLessThanOrEqual(6)
@@ -46,5 +17,9 @@ describe("yams test", () => {
 
   test("return number points equal to 15", () => {
     expect(getPoints([1, 2, 3, 4, 5])).toEqual(15);
+  });
+
+  test("return an empty object if array size is under 5", () => {
+    expect(countDuplicates([1, 1, 2, 2])).toMatchObject({});
   });
 });
